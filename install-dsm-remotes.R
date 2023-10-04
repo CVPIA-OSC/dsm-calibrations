7297d4e84c6feca1f0832eaf40eedeec510f20a6
Emanuel Rodriguez <erodriguez@flowwest.com>
2023-10-04 09:47:04 -0700

    add some helpful comments

--- install-dsm-remotes.R
+++ install-dsm-remotes.R
@@ -1,11 +1,14 @@
+# install dep packages
 remotes::install_github("cvpia-osc/DSMhabitat")
 remotes::install_github("cvpia-osc/DSMflow")
 remotes::install_github("cvpia-osc/DSMtemperature")
 remotes::install_github("cvpia-osc/DSMCalibrationData")
 remotes::install_github("cvpia-osc/DSMscenario")
 
+# install dsm packages
 remotes::install_github("cvpia-osc/fallRunDSM@movement-hypothesis")
 remotes::install_github("cvpia-osc/winterRunDSM")
 remotes::install_github("cvpia-osc/springRunDSM@calibration")
 
+# install genetic alog
 install.packages("GA")
